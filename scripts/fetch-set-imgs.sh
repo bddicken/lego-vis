60aa24e1107343b631e6381feb80d520440ba08f
bddicken <bddicken@gmail.com>
2015-03-10 10:42:58 -0700

    add set img fetcher

--- scripts/fetch-set-imgs.sh
+++ scripts/fetch-set-imgs.sh
@@ -8,28 +8,34 @@
 set -o errexit
 set -o nounset
 
+# First arg is sets file
+SETS_FILE=$(realpath $1)
+
 SDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 source "${SDIR}/utils.sh"
 
 DOWNLOADS_URL="http://rebrickable.com/downloads"
 DOWNLOADS_FILE_NAME="downloads.html"
-BASE_IMG_URL="http://rebrickable.com/img/pieces/"
+BASE_PART_IMG_URL="http://rebrickable.com/img/pieces/"
 REGEX="parts_[0-9][0-9]*.zip"
 
 downloadFromURL "${DOWNLOADS_URL}" "${DOWNLOADS_FILE_NAME}"
 extractAllStringsFromFileMatchingRegex "EXTRACTED_DOWNLOAD_URLS" "${DOWNLOADS_FILE_NAME}" "${REGEX}"
 
+mkdir parts
+pushd parts
+
 TMP_ZIP="temp.zip"
-COUNT=0
 for DL_FILE in ${EXTRACTED_DOWNLOAD_URLS}; do
     echo "${DL_FILE}"
-    DOWNLOAD_URL="${BASE_IMG_URL}${DL_FILE}"
+    DOWNLOAD_URL="${BASE_PART_IMG_URL}${DL_FILE}"
 
     downloadFromURL "${DOWNLOAD_URL}" "${TMP_ZIP}"
     extractZip "${TMP_ZIP}"
-    COUNT=$((COUNT+1))
 done
+
+popd
 
 echo "DONE!"
 
