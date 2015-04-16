bb8dad29bdc099944d71bb9e5a7212864408c1c7
bddicken <bddicken@gmail.com>
2015-04-15 19:55:44 -0700

    WIP

--- server/server.js
+++ server/server.js
@@ -1,24 +1,124 @@
-// server.js
+// load_data.js
 //
-// TODO: disable delete requests for production
+// Load comma delimited data into mongodb
 //
-
-// BASE SETUP
-// =============================================================================
-
-var dbConnectString = 'mongodb://localhost/legodb';
 
 var express    = require('express');
 var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
+
+var program     = require('commander');
+var csv         = require("fast-csv");
+var fs          = require('fs');
+var readline    = require('readline');
+
+program
+  .version('0.0.1')
+  .option('-d, --data <fileName>','lego data directory')
+  .parse(process.argv);
+
+if (!program.data) {
+    console.log("must specify all arguments.");
+}
+
+var pieces = {};
+var setPieces = {};
+var sets = {};
+var colors = {};
+
+var fileCount = 4;
+var checkFinished = function(rowType) {
+    console.log("Done processing " + rowType + " elements.");
+    if (--fileCount <= 0) {
+        console.log("Done processing all data elements. Server now ready to handle all requests.");
+    }
+}
+
+////
+//// Sets
+////
+
+var setStream = fs.createReadStream(program.data + '/sets.csv');
+var setDone = false;
+var setCSVStream = csv()
+.on('end', function(line) {
+    setDone = true;
+    checkFinished("Set");
+})
+.on('data', function(line) {
+    var set = {};
+    set.id     = line[0];
+    set.year   = line[1];
+    set.peices = line[2];
+    set.t1     = line[3];
+    set.descr  = line[4];
+    sets[set.id] = set;
+});
+setStream.pipe(setCSVStream);
+
+////
+//// setPeices
+////
+
+var setPieceStream = fs.createReadStream(program.data + '/set_pieces.csv');
+var setPieceDone = false;
+var setPieceCSVStream = csv()
+.on('end', function(line) {
+    setPieceDone = true;
+    checkFinished("SetPiece");
+})
+.on('data', function(line) {
+    var setPiece = {};
+    setPiece.set_id   = line[0];
+    setPiece.piece_id = line[1];
+    setPiece.num      = line[2];
+    setPiece.color    = line[3];
+    setPiece.type     = line[4];
+    setPieces[setPiece.set_id + "+" + setPiece.piece_id] = setPiece;
+});
+setPieceStream.pipe(setPieceCSVStream);
+
+////
+//// peices
+////
+
+var pieceStream = fs.createReadStream(program.data + '/pieces.csv');
+var pieceDone = false;
+var pieceCSVStream = csv()
+.on('end', function(line) {
+    pieceDone = true;
+    checkFinished("piece");
+})
+.on('data', function(line) {
+    var piece = {}; 
+    piece.id       = line[0];
+    piece.descr    = line[1];
+    piece.category = line[2];
+    pieces[piece.id] = piece;
+});
+pieceStream.pipe(pieceCSVStream);
+
+////
+//// color
+////
+
+var colorStream = fs.createReadStream(program.data + '/colors.csv');
+var colorDone = false;
+var colorCSVStream = csv()
+.on('end', function(line) {
+    colorDone = true;
+    checkFinished("Color");
+})
+.on('data', function(line) {
+    var color = {}; 
+    color.id    = line[0];
+    color.descr = line[1];
+    colors[color.id] = color;
+});
+colorStream.pipe(colorCSVStream);
+
+//////////////////////////// NOW FOR THE SERVER /////////////////////////////
+
 var app        = express();
-
-var Set      = require('./app/models/set');
-var Piece    = require('./app/models/piece');
-var SetPiece = require('./app/models/setPiece');
-var Color    = require('./app/models/color');
-
-
 var maxReturn = 100000000;
 
 // configure app to use bodyParser()
@@ -27,15 +127,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 9090;
-
-mongoose.connect(dbConnectString); 
-
-// ROUTES FOR OUR API
-// =============================================================================
-
 var router = express.Router();              
 
-// middleware t use for all requests
+// middleware to use for all requests
 router.use(function(req, res, next) {
     console.log('Handling generic request.');
 
@@ -52,52 +146,12 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to the api!' });   
 });
 
-// All routes here
-
-router.route('/pieces/:id')
+router.route('/pieces')
 .get(function(req, res) {
-    var id = req.params.id;
-    var pieceQ = Piece.find({"id":id}).limit(maxReturn)
-    pieceQ.exec(function(err, pieces) {
-        if (err) { res.send(err); }
-        res.json(pieces);
-    });
+    res.json(pieces);
 });
 
-router.route('/setPieces/set/:set_id')
-.get(function(req, res) {
-    var set_id = req.params.set_id;
-    var setPQ = SetPiece.find({"set_id":set_id}).limit(maxReturn)
-    setPQ.exec(function(err, setPieces) {
-        if (err) { res.send(err); }
-        res.json(setPieces);
-    });
-});
-
-router.route('/sets')
-.get(function(req, res) {
-    Set.find(function(err, sets) {
-        if (err) { res.send(err); }
-        res.json(sets);
-    });
-});
-
-router.route('/sets/:id')
-.get(function(req, res) {
-    var id = req.params.id;
-    var setQ = Set.find({"id":id}).limit(maxReturn)
-    setQ.exec(function(err, sets) {
-        if (err) { res.send(err); }
-        res.json(sets);
-    });
-});
-
-// REGISTER OUR ROUTES -- all of our routes will be prefixed with /api
 app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-
 app.listen(port);
 console.log('Server running on port ' + port);
 
