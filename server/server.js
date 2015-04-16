8402c6f4d0740eb5dda04e99d5e7c3e643f315b7
Rachel Baumann <rnb137@email.arizona.edu>
2015-04-16 15:54:17 -0700

    Added scatter plots

--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@
 // Load comma delimited data into mongodb
 //
 
+// BASE SETUP
+// =============================================================================
+
+var dbConnectString = 'mongodb://localhost/legodb';
+
 var express    = require('express');
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var app        = express();
 
 var program     = require('commander');
 var csv         = require("fast-csv");
@@ -127,6 +134,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 9090;
+
+mongoose.connect(dbConnectString); 
+
+// ROUTES FOR OUR API
+// =============================================================================
+
 var router = express.Router();              
 
 // middleware to use for all requests
@@ -151,7 +164,40 @@ router.route('/pieces')
     res.json(pieces);
 });
 
+router.route('/setPieces/set/:set_id')
+.get(function(req, res) {
+    var set_id = req.params.set_id;
+    var setPQ = SetPiece.find({"set_id":set_id}).limit(maxReturn)
+    setPQ.exec(function(err, setPieces) {
+        if (err) { res.send(err); }
+        res.json(setPieces);
+    });
+});
+
+router.route('/sets')
+.get(function(req, res) {
+    Set.find(function(err, sets) {
+        if (err) { res.send(err); }
+        res.json(sets);
+    });
+});
+
+router.route('/sets/:id')
+.get(function(req, res) {
+    var id = req.params.id;
+    var setQ = Set.find({"id":id}).limit(maxReturn)
+    setQ.exec(function(err, sets) {
+        if (err) { res.send(err); }
+        res.json(sets);
+    });
+});
+
+// REGISTER OUR ROUTES -- all of our routes will be prefixed with /api
 app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+
 app.listen(port);
 console.log('Server running on port ' + port);
 
